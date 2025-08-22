eea3356fb28d57116bb623e02d9bb3d31be230db
Hazel Lim <hazellim2895@gmail.com>
2025-08-22 14:58:04 +0800

    Added javascript to toggle project descriptions

---
+++ script.js
@@ -0,0 +1,35 @@
+const project1 = document.getElementById('project-1');
+const project2 = document.getElementById('project-2');
+const project3 = document.getElementById('project-3');
+
+project1.addEventListener('click', function() {
+    let projectDescr1 = document.getElementById('project-descrp-1');
+    if (projectDescr1.style.display === 'none') {
+        projectDescr1.style.display = 'block';
+    } else {
+        projectDescr1.style.display = 'none'
+    };
+});
+
+project2.addEventListener('click', function() {
+    let projectDescr2 = document.getElementById('project-descrp-2');
+    if (projectDescr2.style.display === 'none') {
+        projectDescr2.style.display = 'block';
+    } else {
+        projectDescr2.style.display = 'none'
+    };
+});
+
+
+project3.addEventListener('click', function() {
+    let projectDescr3 = document.getElementById('project-descrp-3');
+    if (projectDescr3.style.display === 'none') {
+        projectDescr3.style.display = 'block';
+    } else {
+        projectDescr3.style.display = 'none'
+    };
+});
+
+
+
+
