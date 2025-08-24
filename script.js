f24976694cb0bd01d5393f2863c7b5effcdad37e
Hazel Lim <hazellim2895@gmail.com>
2025-08-24 16:44:30 +0800

    Modified nav elements position to inline and color on hover

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-// Add event listeners to toggle project descriptions
+// Get project elements by id
 const project1 = document.getElementById('project-1');
 const project2 = document.getElementById('project-2');
 const project3 = document.getElementById('project-3');
 
+// Helper function to toggle project descriptions
+const toggleDescrp = element => {
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    };
+}
+
+// Add event listeners to project elements
 project1.addEventListener('click', function() {
     let projectDescr1 = document.getElementById('project-descrp-1');
-    if (projectDescr1.style.display === 'none') {
-        projectDescr1.style.display = 'block';
-    } else {
-        projectDescr1.style.display = 'none'
-    };
+    toggleDescrp(projectDescr1);
 });
 
 project2.addEventListener('click', function() {
     let projectDescr2 = document.getElementById('project-descrp-2');
-    if (projectDescr2.style.display === 'none') {
-        projectDescr2.style.display = 'block';
-    } else {
-        projectDescr2.style.display = 'none'
-    };
+    toggleDescrp(projectDescr2);
 });
 
 
 project3.addEventListener('click', function() {
     let projectDescr3 = document.getElementById('project-descrp-3');
-    if (projectDescr3.style.display === 'none') {
-        projectDescr3.style.display = 'block';
-    } else {
-        projectDescr3.style.display = 'none'
-    };
+    toggleDescrp(projectDescr3);
 });
 
 // Toggle Dark Mode
