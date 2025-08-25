a9b8786a6ff23fd4d2687e8ddc2a7e799f058213
Hazel Lim <hazellim2895@gmail.com>
2025-08-25 16:07:54 +0800

    Added a show class and used classList.toggle('show') on the project descriptions

--- script.js
+++ script.js
@@ -3,31 +3,34 @@ const project1 = document.getElementById('project-1');
 const project2 = document.getElementById('project-2');
 const project3 = document.getElementById('project-3');
 
+let projectDescr1 = document.getElementById('project-descrp-1'); 
+let projectDescr2 = document.getElementById('project-descrp-2');
+let projectDescr3 = document.getElementById('project-descrp-3');
+
 // Helper function to toggle project descriptions
-const toggleDescrp = element => {
-    if (element.style.display === 'none') {
-        element.style.display = 'block';
-    } else {
-        element.style.display = 'none';
-    };
-}
+// const toggleDescrp = element => {
+//     if (element.style.display === 'none') {
+//         element.style.display = 'block';
+//     } else {
+//         element.style.display = 'none';
+//     };
+// }
+
+
 
 // Add event listeners to project elements
 project1.addEventListener('click', function() {
-    let projectDescr1 = document.getElementById('project-descrp-1');
-    toggleDescrp(projectDescr1);
+    projectDescr1.classList.toggle('show');
 });
-
 project2.addEventListener('click', function() {
-    let projectDescr2 = document.getElementById('project-descrp-2');
-    toggleDescrp(projectDescr2);
+    projectDescr2.classList.toggle('show');
 });
-
-
 project3.addEventListener('click', function() {
-    let projectDescr3 = document.getElementById('project-descrp-3');
-    toggleDescrp(projectDescr3);
+    projectDescr3.classList.toggle('show');
 });
+
+
+
 
 // Toggle Dark Mode
 const toggleButton = document.getElementById('toggle-dark-mode');
